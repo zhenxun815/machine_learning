adfbf3bc0801ff551082a3efcfc4f54d0ba7a3cf
zhenxun815 <guoyiheng89@gmail.com>
2019-08-20 15:04:14 +0800

    add images pretreatment

--- threshold/traffic_signs.py
+++ threshold/traffic_signs.py
@@ -8,7 +8,11 @@
 # @Email: GuoYiheng89@gmail.com
 # @Time: 8/19/2019 17:42
 import os
-from skimage.data import imread
+import numpy as np
+from skimage import transform
+from skimage import color
+from skimage import data
+import matplotlib.pyplot as plt
 
 
 def is_dir(parent_path, son_path):
@@ -24,16 +28,17 @@ def list_dir(parent_path, son_path=None):
 def read_img(data_dir, img_dir, img_fname):
     img_dir_path = os.path.join(data_dir, img_dir)
     img_path = os.path.join(img_dir_path, img_fname)
-    return imread(img_path)
+    return data.imread(img_path)
 
 
 def get_data_tuple(data_dir, img_dir, img_fname):
+    # print(f'{data_dir}, {img_dir}, {img_fname}')
     return int(img_dir), read_img(data_dir, img_dir, img_fname)
 
 
 def load_data(data_dir):
-    datas = [get_data_tuple(data_dir, son_file, grand_son) for son_file in list_dir(data_dir) if
-             is_dir(data_dir, son_file)
+    datas = [get_data_tuple(data_dir, son_file, grand_son)
+             for son_file in list_dir(data_dir) if is_dir(data_dir, son_file)
              for grand_son in list_dir(data_dir, son_file) if grand_son.endswith('ppm')]
     """
     for img, label in datas:
@@ -43,9 +48,49 @@ def load_data(data_dir):
     return zip(*datas)
 
 
+def show_img(images, img_id):
+    # Fill out the subplots with the random images that you defined
+    for i in range(len(img_id)):
+        plt.subplot(1, 4, i + 1)
+        plt.axis('off')
+        img2show = images[img_id[i]]
+        plt.imshow(img2show)
+        plt.subplots_adjust(wspace=0.5)
+        plt.show()
+        print(f'shape: {img2show.shape}, min: {img2show.min()}, max: {img2show.max()}')
+
+
+def show_label_img(labels, images, cmap=None):
+    unique_labels = set(labels)
+    plt.figure(figsize=(18, 16))
+    for i, label in enumerate(unique_labels):
+        plt.subplot(8, 8, i + 1)
+        plt.axis('off')
+        img2show = images[labels.index(label)]
+        plt.title(f'label {label}, img count: {labels.count(label)}')
+        plt.imshow(img2show, cmap=cmap)
+    plt.show()
+
+
+def pretreate_image(images):
+    # resize
+    images = [transform.resize(image, (28, 28)) for image in images]
+    images_arr = np.array(images)
+    print(f'image array ndim: {images_arr.ndim}, shape: {images_arr.shape}, size: {images_arr.size}')
+    # grayscale
+    return color.rgb2gray(images_arr)
+
+
 if __name__ == '__main__':
     img_root_path = 'E:/tf_test'
     train_dir = os.path.join(img_root_path, 'Training')
     test_dir = os.path.join(img_root_path, 'Testing')
 
     train_labels, train_images = load_data(train_dir)
+
+    # print(f'{train_images[0]}')
+    # plt.hist(train_labels, 62)
+    # plt.show()
+    # show_img(train_images, [300, 2250, 3650, 4000])
+    train_images = pretreate_image(train_images)
+    show_label_img(train_labels, train_images,cmap='gray')
