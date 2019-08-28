c534071f1d934d3e143ff2809c6b0d8401c8094b
zhenxun815 <guoyiheng89@gmail.com>
2019-08-28 18:53:59 +0800

    classify quality

--- kr/wine_quality.py
+++ kr/wine_quality.py
@@ -18,7 +18,8 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Model
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, cohen_kappa_score
-
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import r2_score
 
 def print_df(df, info=False, head=True, describe=False, isna=False):
     print(f'ndim: {df.ndim}, shape: {df.shape}, size: {df.size}')
@@ -109,6 +110,32 @@ def print_evaluate(y_true, y_pred):
     print(f'{cohen_kappa_score(y_true, y_pred)}')
 
 
+def classify_wine_type(wines_df):
+    x = wines_df.iloc[:, 0:11]
+    y = np.ravel(wines_df.type)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+    scaler = StandardScaler().fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    model = Sequential()
+    model.add(Dense(12, activation='relu', input_shape=(11,)))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # print_model(model)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, batch_size=30, verbose=2)
+    # Return the prediction score
+    # y_pred = model.predict(X_test, verbose=1)
+    # Return the prediction class
+    y_pred = model.predict_classes(x_test, verbose=1)
+
+    # The score is a list that holds the combination of the loss and the accuracy
+    score = model.evaluate(x_test, y_test, verbose=1)
+    print(f'score is: {score}')
+
+
 if __name__ == '__main__':
 
     white = pd.read_csv('winequality-white.csv', sep=';')
@@ -120,26 +147,24 @@ if __name__ == '__main__':
     print_df(wines, describe=True)
     # show_alcohol(white, red)
     # show_sulfate_quality(white, red)
-    X = wines.iloc[:, 0:11]
-    y = np.ravel(wines.type)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    # classify_wine_type(wines)
+    y = wines['quality']
+    x = wines.drop('quality', axis=1)
+    x = StandardScaler().fit_transform(x)
 
-    scaler = StandardScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
 
-    model = Sequential()
-    model.add(Dense(12, activation='relu', input_shape=(11,)))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # print_model(model)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=5, batch_size=30, verbose=2)
-    # Return the prediction score
-    # y_pred = model.predict(X_test, verbose=1)
-    # Return the prediction class
-    y_pred = model.predict_classes(X_test, verbose=1)
+    seed = 7
+    np.random.seed(seed)
 
-    # The score is a list that holds the combination of the loss and the accuracy
-    score = model.evaluate(X_test, y_test, verbose=1)
-    print(f'score is: {score}')
+    kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+    for train, test in kfold.split(x, y):
+        model = Sequential()
+        model.add(Dense(64, activation='relu', input_shape=(12,)))
+        model.add(Dense(1))
+        model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+        model.fit(x[train], y[train], epochs=10, verbose=1)
+
+        mse_value, mae_value = model.evaluate(x[test], y[test], verbose=0)
+        print(f'mse_value is: {mse_value}')
+        print(f'mae_value is: {mae_value}')
+        #r2_score(y[test], y_pred)
